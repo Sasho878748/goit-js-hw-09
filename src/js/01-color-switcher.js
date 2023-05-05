4bb17afb4ad0b6196978d96d69a8a39ccb903c1b
Sasho878748 <kovalenko878748@gmail.com>
2023-05-05 21:21:48 +0300

    create task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
 
+// console.log(startBtn);
+const body = document.body;
+
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function startChangingColor() {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+}
+
+function stopChangingColor() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+}
+
+startBtn.addEventListener("click", startChangingColor);
+stopBtn.addEventListener("click", stopChangingColor);
